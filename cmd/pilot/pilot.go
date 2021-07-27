ae583241014f3438645d081180efaf4077f3b4a3
Bartek Pacia <barpac02@gmail.com>
2021-07-27 09:45:26 +0200

    [WIP]: add write timeouts

--- cmd/pilot/pilot.go
+++ cmd/pilot/pilot.go
@@ -3,11 +3,24 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 5 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 10 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
 )
 
 var (
@@ -42,17 +55,39 @@ func main() {
 }
 
 func inputAndSend(ws *websocket.Conn) {
+	// from here: https://github.com/gorilla/websocket/blob/c3dd95aea9779669bb3daafbd84ee0530c8ce1c1/examples/chat/client.go
+	ticker := time.NewTicker(pingPeriod)
+	commandChan := make(chan struct{})
+
 	for {
 		reader := bufio.NewReader(os.Stdin)
-		input, err := reader.ReadByte()
+		b, err := reader.ReadByte()
 		if err != nil {
-			log.Fatalln("failed to read from stdin:", err)
+			log.Fatalln("read from stdin:", err)
 		}
 
-		err = ws.WriteMessage(websocket.TextMessage, []byte{input})
-		if err != nil {
-			log.Fatalln("failed to write message to websocket connection:", err)
-			return
+		go send(ws, []byte{b}, commandChan)
+
+		select {
+		case <-commandChan:
+			log.Println("command sent successfully")
+		case <-ticker.C:
+			ws.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := ws.WriteMessage(websocket.PingMessage, nil); err != nil {
+				log.Fatalln("failed to write ping message")
+			}
 		}
 	}
+}
+
+func send(ws *websocket.Conn, data []byte, c chan struct{}) error {
+	defer func() {
+		c <- struct{}{}
+	}()
+
+	if err := ws.WriteMessage(websocket.TextMessage, data); err != nil {
+		return fmt.Errorf("write message to websocket connection: %v", err)
+	}
+
+	return nil
 }
